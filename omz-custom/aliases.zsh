6cfea558706736e9e96d3212ac8dad86a1bbfae1
pietvanzoen <pietvanzoen@gmail.com>
2017-02-21 16:36:07 +0100

    add screen -S alias

--- omz-custom/aliases.zsh
+++ omz-custom/aliases.zsh
@@ -3,10 +3,13 @@
 alias zshconfig="vim ~/.zshrc"
 alias ohmyzsh="vim ~/.oh-my-zsh"
 alias reshell="source ~/.zshrc"
-alias sl="screen -ls"
-alias sr="screen -r"
 alias cl=clear
 alias j=z
 
 # SCRIPTS
 alias run-command-on-git-revisions=$DOTFILES/helpers/run-command-on-git-revisions
+
+# SCREEN
+alias sl="screen -ls"
+alias sr="screen -r"
+alias ss="screen -S"
