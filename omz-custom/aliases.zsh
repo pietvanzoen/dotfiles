3f758113fc99e437c3c7c5d99f9599ec4d5576f9
pietvanzoen <pietvanzoen@gmail.com>
2017-06-18 17:59:29 +0200

    free wifi!

--- omz-custom/aliases.zsh
+++ omz-custom/aliases.zsh
@@ -27,3 +27,6 @@ function mktouch() {
 
 # RUBY
 alias bi="bundle check || bundle install"
+
+# FREEWIFI
+alias freewifi="sudo ifconfig en0 ether `openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/.$//'`"
