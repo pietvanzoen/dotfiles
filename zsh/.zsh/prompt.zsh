4a9d5aef3a5c97444cf9988f006dfb4ba3923e73
pietvanzoen <pietvanzoen@gmail.com>
2017-07-04 12:26:00 +0200

    add fzf defaults

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -43,3 +43,11 @@ if [[ ! -f ~/.fzf.zsh ]]; then
 fi
 
 [[ -f ~/.fzf.zsh ]] && source ~/.fzf.zsh
+
+# Setting rg as the default source for fzf
+export FZF_DEFAULT_COMMAND='rg --no-ignore --hidden --files --color=never --glob "!.git/" --glob "!.DS_Store"'
+
+# To apply the command to CTRL-T as well
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+export FZF_DEFAULT_OPTS='--height 40% --reverse'
