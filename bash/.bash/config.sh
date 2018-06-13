7f9c2df1f3b72fc117a55c5cab4e77e5d4750724
pietvanzoen <hi@pietvanzoen.com>
2018-06-13 13:21:40 +0200

    reorganize path config

--- bash/.bash/config.sh
+++ bash/.bash/config.sh
@@ -1,19 +1,27 @@
 # COLORS
 export TERM=xterm-256color
 
-# AUTOJUMP
-[[ -f /usr/local/etc/profile.d/autojump.sh ]] && . /usr/local/etc/profile.d/autojump.sh
-[[ -f $HOME/.autojump/etc/profile.d/autojump.sh ]] && source $HOME/.autojump/etc/profile.d/autojump.sh
+# BIN
+export PATH=$HOME/bin:$PATH
+
+# GNU COMMANDS
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+
+# GO BINARIES
+export GO_PATH="$HOME/go"
+[ -d "$GOPATH/bin" ] && export PATH="$PATH:$GOPATH/bin"
 
 # RVM
 [ -e $HOME/.rvm/scripts/rvm ] && source $HOME/.rvm/scripts/rvm
 [ -d $HOME/.rvm/bin ] && export PATH="$PATH:$HOME/.rvm/bin"
 
+# AUTOJUMP
+[[ -f /usr/local/etc/profile.d/autojump.sh ]] && . /usr/local/etc/profile.d/autojump.sh
+[[ -f $HOME/.autojump/etc/profile.d/autojump.sh ]] && source $HOME/.autojump/etc/profile.d/autojump.sh
+
 # GNU LS colors
 [ -e $HOME/.dir_colors ] && eval "$(dircolors ~/.dir_colors)"
-
-# BIN
-export PATH=$HOME/bin:$PATH
 
 # NVM
 export NVM_DIR="$HOME/.nvm"
@@ -21,10 +29,6 @@ export NVM_DIR="$HOME/.nvm"
 
 # SSH
 [ -e $HOME/.ssh/rsa_id ] && export SSH_KEY_PATH="$HOME/.ssh/rsa_id"
-
-# GO BINARIES
-export GO_PATH="$HOME/go"
-[ -d "$GOPATH/bin" ] && export PATH="$PATH:$GOPATH/bin"
 
 # EDITOR
 export EDITOR="$(which vim)"
@@ -43,10 +47,6 @@ export FZF_DEFAULT_COMMAND='rg --no-ignore --hidden --files --color=never --glob
 # To apply the command to CTRL-T as well
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_DEFAULT_OPTS='--height 40% --reverse'
-
-# GNU COMMANDS
-export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
-export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
 
 # HOMEBREW
 export HOMEBREW_CASK_OPTS="--appdir=/Applications --fontdir=/Library/Fonts"
