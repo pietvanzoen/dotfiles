30e39919cdb69b9d61b98d7ebdefe784e82a0839
pietvanzoen <hi@pietvanzoen.com>
2018-09-06 14:57:38 +0200

    remove usabilla helpers

--- usabilla/.config/bash/autoload/usabilla.sh
+++
@@ -1,9 +0,0 @@
-
-[ -e "$HOME/.usabillarc" ] && source $HOME/.usabillarc
-
-function start-themes-api() {
-  (screen -D -m -SR mongo mongod && \
-  cd ~/Projects/usabilla/themes-api && screen -D -m -SR api yarn start:watch && /
-  cd ~/Projects/usabilla/themes-publisher && screen -D -m -SR pub yarn start:watch && /
-  echo 'themes api started')
-}
