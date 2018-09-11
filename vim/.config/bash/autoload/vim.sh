ea943f296718ffde419ba369a83f6bac6654ef21
pietvanzoen <hi@pietvanzoen.com>
2018-09-11 12:54:11 +0200

    disable nvim for now

--- vim/.config/bash/autoload/vim.sh
+++ vim/.config/bash/autoload/vim.sh
@@ -12,7 +12,7 @@ nn() {
   vim +2 "$(note $title)"
 }
 
-is_executable nvim && alias vim=nvim
+# is_executable nvim && alias vim=nvim
 
 alias vm="vim . -S ~/.vim/sessions/\${PWD##*/}.vim"
 alias vimm="vim -u ~/.vimrc.min"
