a7f60b3e754e62c3ffebea809bf6362f496ccbae
pietvanzoen <hi@pietvanzoen.com>
2018-09-11 16:28:21 +0200

    sandbox is symlinked to home directory

--- bash/.config/bash/autoload/sandbox.sh
+++ bash/.config/bash/autoload/sandbox.sh
@@ -1,18 +1,21 @@
 
 SANDBOXRC=$HOME/.sandboxrc
+SANDBOX_LINK=$HOME/sandbox
 
 if [[ -f "$SANDBOXRC" ]]; then
-  export SANDBOX_PATH="$(cat $SANDBOXRC)"
+  export SANDBOX_TEMP_DIR="$(cat $SANDBOXRC)"
 fi
 
-if [[ ! -d "$SANDBOX_PATH" ]]; then
-  export SANDBOX_PATH="$(mktemp -d)"
-  echo $SANDBOX_PATH > $SANDBOXRC
+if [[ ! -d "$SANDBOX_TEMP_DIR" ]]; then
+  export SANDBOX_TEMP_DIR="$(mktemp -d)"
+  echo $SANDBOX_TEMP_DIR > $SANDBOXRC
+  rm -f $SANDBOX_LINK
+  ln -s $SANDBOX_TEMP_DIR $SANDBOX_LINK
 fi
 
 
 sandbox() {
   local name=$1
-  mkdir -p "$SANDBOX_PATH/$name"
-  cd "$SANDBOX_PATH/$name"
+  mkdir -p "$SANDBOX_LINK/$name"
+  cd "$SANDBOX_LINK/$name"
 }
