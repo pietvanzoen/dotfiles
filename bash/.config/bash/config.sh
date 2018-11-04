9d216e0577dd22533b57758ac1a0f06a55a8988c
pietvanzoen <hi@pietvanzoen.com>
2018-11-04 09:22:52 +0100

    use yarn version shipped with node

--- bash/.config/bash/config.sh
+++ bash/.config/bash/config.sh
@@ -79,7 +79,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # YARN
-export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+#export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 
 # BASH COMPLETION
 if [ -f /usr/local/share/bash-completion/bash_completion ]; then
