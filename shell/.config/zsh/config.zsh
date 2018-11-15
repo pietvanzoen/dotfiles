3908e4e1785e5f8833868d17413fb9d0ca8cc992
pietvanzoen <hi@pietvanzoen.com>
2018-11-15 14:19:35 +0100

    adjust completions format

--- shell/.config/zsh/config.zsh
+++ shell/.config/zsh/config.zsh
@@ -42,8 +42,8 @@ compinit
 
 zstyle ':completion:*' auto-description 'specify: %d'
 zstyle ':completion:*' completer _expand _complete _correct _approximate
-zstyle ':completion:*' format 'Completing %d'
-zstyle ':completion:*' group-name ''
+#zstyle ':completion:*' format 'Completing %d'
+#zstyle ':completion:*' group-name ''
 zstyle ':completion:*' menu select=2
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' list-colors ''
