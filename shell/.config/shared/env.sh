e0f6669496fc899305eae6104a2460770161fbed
pietvanzoen <hi@pietvanzoen.com>
2018-11-15 14:15:42 +0100

    remove redundant autojump source paths

--- shell/.config/shared/env.sh
+++ shell/.config/shared/env.sh
@@ -50,9 +50,7 @@ export GOPATH="$HOME/go"
 [ -d $HOME/.rvm/bin ] && export PATH="$PATH:$HOME/.rvm/bin"
 
 # AUTOJUMP
-[[ -f /etc/profile.d/autojump.sh ]] && . /etc/profile.d/autojump.sh
-[[ -f /usr/local/etc/profile.d/autojump.sh ]] && . /usr/local/etc/profile.d/autojump.sh
-[[ -f $HOME/.autojump/etc/profile.d/autojump.sh ]] && source $HOME/.autojump/etc/profile.d/autojump.sh
+[[ -s $HOME/.autojump/etc/profile.d/autojump.sh ]] && source $HOME/.autojump/etc/profile.d/autojump.sh
 
 # SSH
 [ -e $HOME/.ssh/id_rsa ] && export SSH_KEY_PATH="$HOME/.ssh/id_rsa"
