5009a796fe8d044b72c1e2dbc975fec672ee1ab9
pietvanzoen <hi@pietvanzoen.com>
2018-11-16 12:58:04 +0100

    simplify script helpers

--- _scripts/_helpers.sh
+++ _scripts/_helpers.sh
@@ -14,26 +14,16 @@ _confirm() {
 }
 
 _info() {
-  echo -e "==> $1"
+  echo "==> $1"
 }
 
 _error() {
-  echo -e "${__red}Error: $__color_off$1"
+  echo "Error: $1"
 }
 
 _warn() {
-  echo -e "$__yellow==> $__color_off$1"
+  echo "Warning: $1"
 }
-
-__color_off='\033[0m'       # Text Reset
-__black='\033[0;30m'        # Black
-__red='\033[0;31m'          # Red
-__green='\033[0;32m'        # Green
-__yellow='\033[0;33m'       # Yellow
-__blue='\033[0;34m'         # Blue
-__purple='\033[0;35m'       # Purple
-__cyan='\033[0;36m'         # Cyan
-__white='\033[0;37m'        # White
 
 is_executable() {
   if hash $1 >/dev/null 2>&1; then
