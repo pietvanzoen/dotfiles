c74218f57aa5f4604af8d1b8e5160e18565f7829
pietvanzoen <hi@pietvanzoen.com>
2020-08-21 15:30:01 +0200

    replace nvm with fnm

--- shell/.local/lib/nvm.sh
+++
@@ -1,3 +0,0 @@
-export NVM_DIR="$HOME/.nvm"
-safe_source "$NVM_DIR/nvm.sh"                              # This loads nvm
-is_bash && [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
