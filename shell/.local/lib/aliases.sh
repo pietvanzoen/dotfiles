acd58295e8430fb61ba20313273da991dfb26137
pietvanzoen <hi@pietvanzoen.com>
2021-03-26 17:12:34 +0100

    alias airport command

--- shell/.local/lib/aliases.sh
+++ shell/.local/lib/aliases.sh
@@ -49,3 +49,5 @@ flushdnscache() {
   sudo dscacheutil -flushcache;
   sudo killall -HUP mDNSResponder;
 }
+
+alias airport=/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport
