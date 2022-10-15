5244399e1a08f3ebed670011badc402581d45a0a
pietvanzoen <hi@pietvanzoen.com>
2022-10-15 10:21:09 +0200

    Update ssh to use 1password

--- shell/.local/lib/ssh.sh
+++ shell/.local/lib/ssh.sh
@@ -1,2 +1,5 @@
-[ -e $HOME/.ssh/id_rsa ] && export SSH_KEY_PATH="$HOME/.ssh/id_rsa"
-is_executable ssh-agent && [[ -z "$(pgrep ssh-agent)" ]] && eval "$(ssh-agent)" >/dev/null
+if [ ! -e $HOME/.1password ]; then
+  mkdir -p ~/.1password && ln -s ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock ~/.1password/agent.sock
+fi
+
+export SSH_AUTH_SOCK=~/.1password/agent.sock
