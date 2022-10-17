84a9b0447918d4cc73f44607be6fcc5cad0e89b8
pietvanzoen <hi@pietvanzoen.com>
2022-10-17 10:54:44 +0200

    Fix brew prefix

--- shell/.local/lib/homebrew.sh
+++ shell/.local/lib/homebrew.sh
@@ -1,17 +1,19 @@
 # HOMEBREW PATHS
+export BREW_PREFIX="$(brew --prefix)"
+
 libexec_packages=(coreutils gnu-indent gnu-tar grep gnu-sed gawk gnu-time findutils)
 for pkg in ${libexec_packages[*]}; do
-  add_path "/usr/local/opt/$pkg/libexec/gnubin"
-  add_manpath "/usr/local/opt/$pkg/libexec/gnuman"
+  add_path "$BREW_PREFIX/opt/$pkg/libexec/gnubin"
+  add_manpath "$BREW_PREFIX/opt/$pkg/libexec/gnuman"
 done
 
-bin_packages=(openssl curl gnu-getopt)
+bin_packages=(openssl curl gnu-getopt gnu-sed)
 for pkg in ${bin_packages[*]}; do
-  add_path "/usr/local/opt/$pkg/bin"
-  add_path "/usr/local/opt/$pkg/share/man"
+  add_path "$BREW_PREFIX/opt/$pkg/bin"
+  add_path "$BREW_PREFIX/opt/$pkg/share/man"
 done
 
-export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/opt/openssl/lib/
+export LIBRARY_PATH="$LIBRARY_PATH:$BREW_PREFIX/opt/openssl/lib/"
 
 # OPTIONS
 export HOMEBREW_CASK_OPTS="--appdir=/Applications --fontdir=/Library/Fonts"
