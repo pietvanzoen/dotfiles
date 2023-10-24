d4b05e7a3a8c044586f2b404c759ba8c5818e812
pietvanzoen <hi@piet.me>
2023-10-24 15:44:46 +0200

    Remove hub

--- git/.local/lib/git.sh
+++ git/.local/lib/git.sh
@@ -3,7 +3,7 @@ alias grt=". git-root"
 
 export GIT_PATH="$HOME/repos/" # for git get
 export GIT_GET_DEFAULT_PREFIX="git@github.com:"
-is_executable hub && eval "$(hub alias -s)"
+# is_executable hub && eval "$(hub alias -s)"
 
 gg() {
   dir="$(git get $1)"
