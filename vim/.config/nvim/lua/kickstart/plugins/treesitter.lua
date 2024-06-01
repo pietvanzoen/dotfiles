77f4d85b1319c3963aac504b9760d965789738ef
pietvanzoen <hi@piet.me>
2024-06-01 21:24:38 +0200

    Fix treesitter highlighting

--- vim/.config/nvim/lua/kickstart/plugins/treesitter.lua
+++ vim/.config/nvim/lua/kickstart/plugins/treesitter.lua
@@ -25,12 +25,9 @@ return {
       auto_install = true,
       highlight = {
         enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { "ruby" },
+        additional_vim_regex_highlighting = false,
       },
-      indent = { enable = true, disable = { "ruby" } },
+      indent = { enable = true },
     },
     config = function(_, opts)
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
@@ -39,6 +36,11 @@ return {
       require("nvim-treesitter.install").prefer_git = true
       ---@diagnostic disable-next-line: missing-fields
       require("nvim-treesitter.configs").setup(opts)
+
+      vim.api.nvim_set_hl(0, "@punctuation.bracket", { link = "Variable" })
+      vim.api.nvim_set_hl(0, "@punctuation.delimiter", { link = "Variable" })
+      vim.api.nvim_set_hl(0, "@keyword.function", { link = "Keyword" })
+      vim.api.nvim_set_hl(0, "@include", { link = "Keyword" })
 
       -- There are additional nvim-treesitter modules that you can use to interact
       -- with nvim-treesitter. You should go explore a few and see what interests you:
