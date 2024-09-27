9f2e1e01a6a18d4d16cf82d25bd2dfe874e4bb9c
pietvanzoen <hi@piet.me>
2024-09-26 22:38:50 -0700

    feat: add Aider plugin configuration for Neovim

---
+++ vim/.config/nvim/lua/plugins/aider.lua
@@ -0,0 +1,21 @@
+-- luacheck: globals vim
+local function get_open_buffers_in_current_window()
+  local current_tabpage = vim.api.nvim_get_current_tabpage()
+  local windows = vim.api.nvim_tabpage_list_wins(current_tabpage)
+  local buffers = {}
+
+  for _, win in ipairs(windows) do
+    local buf = vim.api.nvim_win_get_buf(win)
+    local buf_name = vim.api.nvim_buf_get_name(buf)
+    table.insert(buffers, buf_name)
+  end
+
+  return buffers
+end
+
+function open_aider_with_buffers()
+  local buffers = get_open_buffers_in_current_window()
+  vim.cmd("VimuxRunCommand aider " .. table.concat(buffers, " "))
+end
+
+return {}
